f37f89fb81206494bea948e4c6a59050953a03f9
Thomas VERMEILH <tvermeil@student.42.fr>
2015-08-16 15:05:01 +0200

    init de la grille

--- ex00/grid.c
+++ ex00/grid.c
@@ -6,7 +6,7 @@
 /*   By: tvermeil <tvermeil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/15 22:34:56 by tvermeil          #+#    #+#             */
-/*   Updated: 2015/08/15 23:01:01 by tvermeil         ###   ########.fr       */
+/*   Updated: 2015/08/16 15:03:53 by tvermeil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,26 +20,73 @@ t_grid	ft_create_grid(int *lines[])
 	 */
 }
 
-void	ft_init_lines(int *lines[], t_grid *grid)
+void	ft_init_lines(int *lines_param[], struct s_grid *grid)
+/* remplie les lignes de la grille avec des pointeurs a partir du tableau des ints passé en param
+ */
 {
-	/* remplie les lignes de la grille avec des pointeursa partir du tableau des ints passé en param
-	 */
+	int lin;
+	int col;
+
+	lin = 0;
+	while(line < 9)
+	{
+		col = 0;
+		while(col < 9)
+		{
+			grid->lines[lin][col] = &(lines_param[lin + 1][col]);
+			col++;
+		}
+		lin++;
+	}
 }
 
-void	ft_init_columns(int *lines[], t_grid *grid)
+void	ft_init_columns(int *lines_param[], t_grid *grid)
+/* remplie les colonnes de la grille avec des pointeurs a partir du tableau des ints passé en param
+ */
 {
-	/* remplie les colonnes de la grille avec des pointeurs a partir du tableau des ints passé en param
-	 */
+	int lin;
+	int col;
+
+	col = 0;
+	while(col < 9)
+	{
+		lin = 0;
+		while(lin < 9)
+		{
+			grid->columns[lin][col] = &(lines_param[col][lin + 1]);
+			line++;
+		}
+		col++;
+	}
 }
 
 void	ft_init_blocks(int *lines[], t_grid *grid)
+/* remplie les blocks de la grille avec des pointeurs a partir du tableau des ints passé en param
+*/
 {
-	/* remplie les blocks de la grille avec des pointeurs a partir du tableau des ints passé en param
-	 */
+	int lin;
+	int col;
+	int block;
+	int case_in_block;
+
+	lin = 0;
+	while(lin < 9)
+	{
+		col = 0;
+		while(col < 9)
+		{
+			block = ft_get_block_of(col, lin);
+			case_in_block = (x % 3) + 3 * (y % 3);
+			grid->blocks[block][case_in_block] = &(lines[lin + 1][col]);
+			col++;
+		}
+		lin++;
+	}
 }
 
 int		ft_get_block_of(int x, int y)
+/* renvoie le numero du block des coordonnees passées en paramètre
+*/
 {
-	/* renvoie le numero du block des coordonnees passées en paramètre
-	 */
+	return((x / 3) + 3 * (y / 3));
 }
--- ex00/grid.h
+++ ex00/grid.h
@@ -6,16 +6,16 @@
 /*   By: tvermeil <tvermeil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/15 22:34:12 by tvermeil          #+#    #+#             */
-/*   Updated: 2015/08/15 22:34:35 by tvermeil         ###   ########.fr       */
+/*   Updated: 2015/08/16 12:12:25 by tvermeil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	__GRILLE__
 #	define __GRILLE__
-typedef struct	s_grid
+struct	s_grid
 {
-		int	***lines;
-		int	***columns;
-		int	***blocks;
-}	t_grille;
+		int	*lines[9][9];
+		int	*columns[9][9];
+		int	*blocks[9][9];
+};
 #endif
